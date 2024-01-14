f64e591acd8de1a9db01c66c7914b18dea943b13
ivannew <ivaneriquesotorodriguez@gmail.com>
2024-01-13 20:10:55 -0700

    aaa

--- app2/MainPage.xaml.cs
+++ app2/MainPage.xaml.cs
@@ -17,66 +17,53 @@ namespace app2
         public void mostrar(object sender, EventArgs e)
         {
             var nombreText = nombre.Text;
-            string mensaje = nombreText +": ";
+            string mensaje = nombreText + ": ";
 
             if (Hombre.IsChecked == true)
             {
-                if (checkBoxAlto.IsChecked == true)
-                {
-                    mensaje += "alto, ";
-                }
-                if (checkBoxExtrav.IsChecked == true)
-                {
-                    mensaje += "extravago, ";
-                }
-                if (checkBoxFeo.IsChecked == true)
-                {
-                    mensaje += "feo, ";
-                }
-                if (checkBoxGrande.IsChecked == true)
-                {
-                    mensaje += "grande, ";
-                }
-                if (checkBoxListo.IsChecked == true)
-                {
-                    mensaje += "listo, ";
-                }
-                if (checkBoxRaro.IsChecked == true)
-                {
-                    mensaje += "raro, ";
-                }
+                Agregar(checkBoxAlto, ref mensaje, "alto");
+                Agregar(checkBoxExtrav, ref mensaje, "extravo");
+                Agregar(checkBoxFeo, ref mensaje, "feo");
+                Agregar(checkBoxGrande, ref mensaje, "grande");
+                Agregar(checkBoxListo, ref mensaje, "listo");
+                Agregar(checkBoxRaro, ref mensaje, "raro");
             }
             //-------------------------------------------------------------------------------------------------------
 
             else if (Mujer.IsChecked == true)
             {
-                if (checkBoxAlto.IsChecked == true)
-                {
-                    mensaje += "alta, ";
-                }
-                if (checkBoxExtrav.IsChecked == true)
-                {
-                    mensaje += "extravaga, ";
-                }
-                if (checkBoxFeo.IsChecked == true)
-                {
-                    mensaje += "fea, ";
-                }
-                if (checkBoxGrande.IsChecked == true)
-                {
-                    mensaje += "grande, ";
-                }
-                if (checkBoxListo.IsChecked == true)
-                {
-                    mensaje += "lista, ";
-                }
-                if (checkBoxRaro.IsChecked == true)
-                {
-                    mensaje += "rara, ";
-                }
+                Agregar(checkBoxAlto, ref mensaje, "alta");
+                Agregar(checkBoxExtrav, ref mensaje, "extravaga");
+                Agregar(checkBoxFeo, ref mensaje, "fea");
+                Agregar(checkBoxGrande, ref mensaje, "grande");
+                Agregar(checkBoxListo, ref mensaje, "lista");
+                Agregar(checkBoxRaro, ref mensaje, "rara");
+            }
+
+          
+            mensaje = mensaje.TrimEnd(' ', ',');
+            int ultimacoma = mensaje.LastIndexOf(", ");
+            if (ultimacoma >= 0)
+            {
+                mensaje = mensaje.Substring(0, ultimacoma) + " y " + mensaje.Substring(ultimacoma + 2)+".";
             }
 
             todo.Text = mensaje;
         }
+
+        private void Agregar(CheckBox checkBox, ref string mensaje, string palabra)
+        {
+            if (checkBox.IsChecked == true)
+            {
+                if (!string.IsNullOrEmpty(mensaje) && !mensaje.EndsWith(": "))
+                {
+                    mensaje += ", ";
+                }
+
+                mensaje += $"{palabra}";
+            }
+        }
+
+
     }
 }
